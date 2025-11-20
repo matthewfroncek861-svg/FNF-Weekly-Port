0116a8df06e329dbc5e9eae1c45614474e545e73
matthewfroncek861-svg <matthewfroncek861@gmail.com>
2025-11-20 12:55:56 -0500

    Update import.hx

--- source/import.hx
+++ source/import.hx
@@ -47,21 +47,50 @@ import flxanimate.*;
 import flxanimate.PsychFlxAnimate as FlxAnimate;
 #end
 
-//Flixel
-import flixel.sound.FlxSound;
+//flixel
 import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.FlxCamera;
 import flixel.math.FlxMath;
-import flixel.math.FlxPoint;
-import flixel.util.FlxColor;
-import flixel.util.FlxTimer;
 import flixel.text.FlxText;
+import flixel.util.FlxColor;
+import flixel.tweens.FlxTween;
+import flixel.tweens.FlxEase;
+import flixel.util.FlxTimer;
+import flixel.FlxBasic;
+import flixel.math.FlxPoint;
+import flixel.system.FlxSound;
 import flixel.tweens.FlxEase;
 import flixel.tweens.FlxTween;
 import flixel.group.FlxSpriteGroup;
 import flixel.group.FlxGroup.FlxTypedGroup;
 import flixel.addons.transition.FlxTransitionableState;
 
+#if DISCORD_ALLOWED
+import meta.data.Discord;
+#end
+
+import Init;
+
+import hxvlc.flixel.*;
+
+import meta.data.Paths;
+import meta.data.ClientPrefs;
+import meta.data.Conductor;
+import meta.data.CoolUtil;
+import meta.data.Highscore;
+
+import meta.states.*;
+
+import gameObjects.BGSprite;
+
+import meta.states.KUTValueHandler;
+
+import meta.data.ExUtils;
+import meta.data.ExUtils.addShader as ApplyShaderToCamera;
+import meta.data.ExUtils.removeShader as RemoveShaderFromCamera;
+import meta.data.ExUtils.insertFlxCamera as insertFlxCamera;
+
 using StringTools;
+using meta.FlxObjectTools;
 #end
